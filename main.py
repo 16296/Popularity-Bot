4538048fb27a6e4dedab544ba8c84940001675a1
E E <91461728+16296@users.noreply.github.com>
2021-11-16 17:55:34 +0000

    Complete overhaul, entirely object-based now

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ class Party():
 	def __init__(self,name,members,extrapoints):
 		self.name = name
 		self.points = (members*5) + extrapoints
+		self.house = self.points//30 + 1
 
 	def listAttributes(self):
-		return [self.name, self.points]
+		return [self.name, self.points, self.house]
 
-with open("parties.json","r") as partyFile:
-	partyDict = filereader.load(partyFile)
-#partyDict is a two-dimensional dictionary
-#partyDict[i] has keys "name","members","extrapoints"
+def createObjects():
+	global partyObjects
+	with open("parties.json","r") as partyFile:
+		partyDict = filereader.load(partyFile)
+	#partyDict is a two-dimensional dictionary
+	partyRefs = list(partyDict.keys())
+	partyObjects = {}
+	for i,j in zip(range(len(partyRefs)),partyRefs):
+		temp = partyDict[partyRefs[i]].values()
+		valueArray = list(temp)
+		partyObjects[j] = Party(*valueArray) #partyObjects["mp"] for example is the name of the Minarchy Party object and can be called as such
 
-partyRefs = list(partyDict.keys())
+def main():
+	createObjects()
+	print("UNITED PARTIES OF DISCORD ELECTORAL MANAGER\n1. Display party member counts\n2. Input party member counts\n3. ") #unf
 
-print(partyRefs)
-
-for i in partyRefs:
-	print("The",partyDict[i]["name"],"Party has",partyDict[i]["members"],"members.")
+main()

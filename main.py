ca723357f62d14b886119afcbc590a6c4ee7e02e
E E <91461728+16296@users.noreply.github.com>
2021-11-16 10:34:01 +0000

    xyz

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import json as filereader
+
+class Party():
+	def __init__(self,name,members,extrapoints):
+		self.name = name
+		self.points = (members*5) + extrapoints
+
+	def listAttributes(self):
+		return [self.name, self.points]
+
+
+partyList = filereader.load(file open)
